91d735d1e407e5c2cba4d9cb91b820b3fe18b130
Guillermo Ortún Delicado <c3nz@c3nz.local>
2024-02-05 03:14:24 +0100

    .

--- src/philos/philo_dead.c
+++ src/philos/philo_dead.c
@@ -6,11 +6,21 @@
 /*   By: guortun- <guortun-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 17:39:39 by guortun-          #+#    #+#             */
-/*   Updated: 2024/02/04 22:51:42 by guortun-         ###   ########.fr       */
+/*   Updated: 2024/02/05 03:07:42 by guortun-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+static void	print_dead(t_philo *philo, int long ms)
+{
+	if (philo->data->print_ok)
+		return ;
+	pthread_mutex_lock(&philo->data->print);
+	printf("%ld\t%d %s\n", ms, philo->id, "died");
+	philo->data->print_ok = 1;
+	pthread_mutex_unlock(&philo->data->print);
+}
 
 int	dead(t_philo *philo)
 {
@@ -27,13 +37,10 @@ int	dead(t_philo *philo)
 		pthread_mutex_unlock(&philo->data->dead_mutex);
 	if ((ms - philo->last_eat) > philo->data->time_to_die)
 	{
-		print_message(philo, "died", ms);
 		pthread_mutex_lock(&philo->data->dead_mutex);
 		philo->data->dead = 1;
 		pthread_mutex_unlock(&philo->data->dead_mutex);
-		pthread_mutex_lock(&philo->data->print);
-		philo->data->print_ok = 1;
-		pthread_mutex_unlock(&philo->data->print);
+		print_dead(philo, ms);
 		return (1);
 	}
 	return (0);
